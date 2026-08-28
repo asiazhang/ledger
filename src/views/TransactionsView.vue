ba4cd4e133cd0a5d811c6efd2ae77681d6c71206
zhangheng <asiazhang2002@gmail.com>
2026-08-28 22:29:48 +0800

    fix(ui): 右键菜单宽度改用 NDropdown width=180（naive-ui min-width 有 maxWidth 误设 bug）

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -523,7 +523,7 @@ onMounted(() => {
       :x="menuX"
       :y="menuY"
       :options="menuOptions"
-      :min-width="180"
+      :width="180"
       @select="onMenuSelect"
       @clickoutside="menuShow = false"
     />
